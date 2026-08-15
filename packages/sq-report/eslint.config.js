ab5dd30a958e7b1dbb73059a2f688bade31502f7
Jonathan Rehem <jonathan.rehem@outlook.com>
2026-08-15 07:40:49 -0300

    drill(document): Purge last stale lavish references: eslint ignore and issue template

--- packages/sq-report/eslint.config.js
+++ packages/sq-report/eslint.config.js
@@ -6,7 +6,7 @@ import globals from "globals";
 
 export default defineConfig(js.configs.recommended, eslintConfigPrettier, {
   files: ["**/*.js"],
-  ignores: ["dist/", "node_modules/", ".sq-report/", "lavish-code-review.html"],
+  ignores: ["dist/", "node_modules/", ".sq-report/", "thread-board.html"],
   languageOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
