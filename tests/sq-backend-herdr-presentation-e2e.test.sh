764600bc202c080176a756ece157f5f07841025c
Jonathan Rehem <jonathan.rehem@outlook.com>
2026-09-10 05:25:33 -0300

    drill: apply CI fixes

--- tests/sq-backend-herdr-presentation-e2e.test.sh
+++ tests/sq-backend-herdr-presentation-e2e.test.sh
@@ -403,6 +403,7 @@ teardown_task() {  # <id> <home>
 normalize_meta() {  # <meta>
   sed -E \
     -e 's|^window=.*$|window=<herdr-container-id>|' \
+    -e 's|^endpoint_task_id=.*$|endpoint_task_id=<task-id>|' \
     -e 's|^herdr_workspace_id=.*$|herdr_workspace_id=<herdr-container-id>|' \
     -e 's|^herdr_tab_id=.*$|herdr_tab_id=<herdr-container-id>|' \
     -e 's|^herdr_pane_id=.*$|herdr_pane_id=<herdr-container-id>|' \
